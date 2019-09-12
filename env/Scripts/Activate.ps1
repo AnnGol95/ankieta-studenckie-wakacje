9f38be1b869f675c624e40bc0de400cf74195924
overment <anna.golinska.95@gmail.com>
2019-09-12 17:38:36 +0200

    update

--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Anna\Documents\GitHub\ankieta-studenckie-wakacje\env"
+$env:VIRTUAL_ENV="C:\Users\Anna\Desktop\ankieta-studenckie-wakacje-master\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
